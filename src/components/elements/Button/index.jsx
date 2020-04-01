479fefedbe5bba9792e89cbf9eef4881d6cc0518
Brijesh Pant <pantbrijesh717@live.com>
2020-04-01 23:38:47 +0530

    add ThemedComponent to support theme based styling

--- src/components/elements/Button/index.jsx
+++ src/components/elements/Button/index.jsx
@@ -2,31 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import cs from 'classnames'
 
+import ThemedComponent from '../../common/ThemedComponent';
+
 import styles from '../../styles/button.css';
 
 function Button(props) {
-    const {
-        color,
-        children,
-        size,
-        disabled,
-    } = props;
-
-    const classes = cs(styles.button, {
-        [styles.primary]: color === 'primary',
-        [styles.stroke]: color === 'stroke',
-        [styles.small]: size === 'small',
-        [styles.large]: size === 'large',
-        [styles.disabled]: disabled,
-    })
-
     return (
-        <button
-            disabled={disabled}
-            className={classes}
-        >
-            {children}
-        </button>
+        <ThemedComponent as='button' styles={styles} {...props} />
     )
 }
 
