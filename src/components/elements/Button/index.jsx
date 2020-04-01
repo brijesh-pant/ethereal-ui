343710b735ab0c52746f4795028bc4af430db280
Brijesh Pant <pantbrijesh717@live.com>
2020-04-01 23:00:40 +0530

    basic button prop based style variations

--- src/components/elements/Button/index.jsx
+++ src/components/elements/Button/index.jsx
@@ -1,19 +1,39 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import cs from 'classnames'
 
 import styles from '../../styles/button.css';
 
 function Button(props) {
+    const {
+        color,
+        children,
+        size,
+        disabled,
+    } = props;
+
+    const classes = cs(styles.button, {
+        [styles.primary]: color === 'primary',
+        [styles.stroke]: color === 'stroke',
+        [styles.small]: size === 'small',
+        [styles.large]: size === 'large',
+        [styles.disabled]: disabled,
+    })
+
     return (
-        <button className={styles.button}>
-            {props.children}
+        <button
+            disabled={disabled}
+            className={classes}
+        >
+            {children}
         </button>
     )
 }
 
 Button.propTypes = {
-
+    color: PropTypes.string,
+    size: PropTypes.string,
+    disabled: PropTypes.bool,
 }
 
 export default Button
-
