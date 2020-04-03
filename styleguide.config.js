12421a88f630fc7db53a1652d51fe7075755abd6
Brijesh Pant <pantbrijesh717@live.com>
2020-04-03 23:45:57 +0530

    classify component as sections in styleguid

--- styleguide.config.js
+++ styleguide.config.js
@@ -6,6 +6,10 @@ const { version } = require('./package');
 module.exports = {
 	title: 'Ethereal UI',
 	version,
+	require: [path.join(__dirname, 'src/components/styles/theme.css')],
+	sections: [
+		{ name: 'Elements', components: 'src/components/elements/**/*.jsx' },
+	  ],
 	webpackConfig: {
 		module: {
 			rules: [
