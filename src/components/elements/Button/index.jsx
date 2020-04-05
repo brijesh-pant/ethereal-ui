22e5a2801e6038fa5f5fe5a111cfe7a60ea880f7
Brijesh Pant <pantbrijesh717@live.com>
2020-04-05 16:42:33 +0530

    update button styleguide (#6)
    * update button styleguide
    
    * add isFullWidth button prop
    
    * add Icon component
    
    * add icon and button with icon component

--- src/components/elements/Button/index.jsx
+++ src/components/elements/Button/index.jsx
@@ -1,20 +1,77 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import cs from 'classnames'
 
 import ThemedComponent from '../../../hoc/ThemedComponent';
 
 import styles from '../../styles/button.css';
 
 const Button = (props) => {
+    const {
+        leftIcon,
+        rightIcon,
+        icon,
+        children,
+    } = props;
+
+    const renderLeftIcon = () => {
+        const {
+            children: iconChildren
+        } = leftIcon.props;
+        return React.cloneElement(leftIcon, {
+            className: cs(styles.icon, styles.leftIcon)
+        }, iconChildren)
+    }
+
+    const renderRightIcon = () => {
+        const {
+            children: iconChildren
+        } = rightIcon.props;
+        return React.cloneElement(rightIcon, {
+            className: cs(styles.icon, styles.rightIcon)
+        }, iconChildren)
+    }
+
     return (
-        <ThemedComponent as='button' styles={styles} {...props} />
+        <ThemedComponent
+            as='button'
+            className={cs({[styles.iconAndLabelButton]: leftIcon || rightIcon})}
+            styles={styles}
+            {...props}
+        >
+            {leftIcon && renderLeftIcon()}
+            {children}
+            {rightIcon && renderRightIcon()}
+        </ThemedComponent>
     )
 }
 
+Button.defaultProps = {
+    color: 'default',
+    size: 'medium',
+    leftIcon: null,
+    rightIcon: null,
+}
+
 Button.propTypes = {
-    color: PropTypes.string,
-    size: PropTypes.string,
+    color: PropTypes.oneOf([
+        'default',
+        'primary',
+        'stroke',
+        'success',
+        'info',
+        'warning',
+        'danger',
+    ]),
+    size: PropTypes.oneOf([
+        'small',
+        'medium',
+        'large',
+    ]),
     disabled: PropTypes.bool,
+    isFullWidth: PropTypes.bool,
+    leftIcon: PropTypes.node,
+    rightIcon: PropTypes.node,
 }
 
 export default Button
